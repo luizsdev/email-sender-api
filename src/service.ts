72fbc146d2ad0a7ca1180a98e4d2dfe2a89b5d15
luizsdev <luizsdev@gmail.com>
2022-07-07 16:00:23 -0300

    import express spec

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,6 @@
+import { Request,Response } from "express"
 import app from  "./init"
 
-app.get("/alo",(req,res)=>{
+app.get("/alo",(req: Request,res: Response)=>{
     res.send("Working fine!")
 })
